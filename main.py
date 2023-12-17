0fc3cc6928810b3fd4ef2ecef2060d454ba65782
zayn1ddinovna <ashrboyeva@gmail.com>
2023-12-17 16:19:19 +0500

    finished: 16:16

---
+++ main.py
@@ -0,0 +1,21 @@
+#  def task2(num):
+#    c=num//5
+#     if num>5:
+#         print(f"we need {c} car")
+#     else:
+#         print(f"we haven't got enough people")
+#         return
+#
+# # print(task2(num))
+#task
+# def numbers(num):
+#     for x in num:
+#         print((f"{x % 50} foizli"))
+#
+# function = [10, 7, 3]
+# numbers(function)
+#task1
+# for x in range (1,11):
+#     if not x in massive:
+#       return x
+
